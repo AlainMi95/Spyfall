c0159a0d63070d98114a3e3d4efdd9a7b0358328
AlainMich <michiala@students.zhaw.ch>
2021-05-04 20:30:18 +0200

    Basic Layout

--- app/src/main/java/com/example/spyfall/MainActivity.kt
+++ app/src/main/java/com/example/spyfall/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.spyfall
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        title = "Spyfall"
+        val textView: TextView = findViewById(R.id.textViewLink)
+        textView.movementMethod = LinkMovementMethod.getInstance()
     }
 }
